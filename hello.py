c72392477362ddd255c73c30c62f889041617da7
Lei Xin <chjgreen@foxmail.com>
2018-08-31 17:28:30 +0800

    使用bootstrap插件优化前端

--- hello.py
+++ hello.py
@@ -1,19 +1,30 @@
-from flask import Flask
-from flask_script import Manager
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 
-manager = Manager(app)
+bootstrap = Bootstrap(app)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
 
 
 @app.route('/')
 def index():
-    return '<h1>Hello World!</h1>'
+    return render_template('index.html')
 
 
 @app.route('/user/<name>')
 def user(name):
-    return '<h1>hello, %s!</h1>' % name
+    return render_template('user.html', name=name)
 
-if __name__ == '__main__':
-    manager.run()
+if __name__ == "__main__":
+    app.run()
+
